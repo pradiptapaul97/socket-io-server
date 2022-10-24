6f6440df0deb46847caa32bcf178ff1b2e062869
pradiptapaul97 <ppradipta65@gmail.com>
2022-10-24 21:54:47 +0530

    first push

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require("http");
+const app = express();
+require('dotenv').config();
+const server = http.createServer(app);
+server.listen(3000);
+//for accept client origin else cors error
+const io = socketIO(server,{
+    cors:{
+        // origin:["http://localhost:8080"]
+        origin:"*"
+    }
+});
+
+//soket io connection
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('send-messege',(messege,room)=>{
+        console.log(messege);
+        //create receive-messege event
+        //io.emit('receive-messege',messege);  //for all user include the sender
+        //socket.broadcast.emit('receive-messege',messege); // all user except sender 
+        if(room)
+        {
+            socket.to(room).emit('receive-messege',messege);
+        }
+        else
+        {
+            socket.broadcast.emit('receive-messege',messege);
+        }
+    })
+    socket.on('join-room',(room,cb)=>{
+        console.log(room);
+        if(room)
+        {
+            socket.join(room);
+            cb(`Joined ${room}`)
+        }
+    })
+  });
+  
+
+app.get('/',(req,res)=>{
+    res.send(`Socket connected`);
+})
+
+console.log(`app is running on http://localhost:3000`);
