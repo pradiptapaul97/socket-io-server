a006230f0c4bab3fa6fb1ef49adcd867e833a084
pradiptapaul97 <ppradipta65@gmail.com>
2022-10-25 20:51:22 +0530

    port structure change

--- server.js
+++ server.js
@@ -1,48 +1,50 @@
 const express = require('express');
-const socketIO = require('socket.io');
-const http = require("http");
 const app = express();
 require('dotenv').config();
-const server = http.createServer(app);
-server.listen(process.env.PORT || 3000);
+const http = require("http").createServer(app);
 //for accept client origin else cors error
-const io = socketIO(server,{
+const socketIO = require('socket.io')(http,{
     cors:{
         // origin:["http://localhost:8080"]
         origin:"*"
     }
 });
 
-//soket io connection
-io.on('connection', (socket) => {
-    console.log(socket.id);
-    socket.on('send-messege',(messege,room)=>{
-        console.log(messege);
-        //create receive-messege event
-        //io.emit('receive-messege',messege);  //for all user include the sender
-        //socket.broadcast.emit('receive-messege',messege); // all user except sender 
-        if(room)
-        {
-            socket.to(room).emit('receive-messege',messege);
-        }
-        else
-        {
-            socket.broadcast.emit('receive-messege',messege);
-        }
-    })
-    socket.on('join-room',(room,cb)=>{
-        console.log(room);
-        if(room)
-        {
-            socket.join(room);
-            cb(`Joined ${room}`)
-        }
-    })
-  });
+http.listen(process.env.PORT || 4000,()=>{
+    //soket io connection
+    socketIO.on('connection', (socket) => {
+        console.log(socket.id);
+        socket.on('send-messege',(messege,room)=>{
+            console.log(messege);
+            //create receive-messege event
+            //io.emit('receive-messege',messege);  //for all user include the sender
+            //socket.broadcast.emit('receive-messege',messege); // all user except sender 
+            if(room)
+            {
+                socket.to(room).emit('receive-messege',messege);
+            }
+            else
+            {
+                socket.broadcast.emit('receive-messege',messege);
+            }
+        })
+        socket.on('join-room',(room,cb)=>{
+            console.log(room);
+            if(room)
+            {
+                socket.join(room);
+                cb(`Joined ${room}`)
+            }
+        })
+    });
+    console.log(`app is running on http://localhost:4000`);
+});
+
+
   
 
 app.get('/',(req,res)=>{
     res.send(`Socket connected`);
 })
 
-console.log(`app is running on http://localhost:3000`);
+
